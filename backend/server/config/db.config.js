cf7eecd52dcd753a531faaeec4d371d60d927e20
Derek-Martineau <derekmartineau@hotmail.com>
2023-01-24 11:37:20 -0500

    feat(server) Both front and back-end servers are running and communicating with eachother

--- backend/server/config/db.config.js
+++ backend/server/config/db.config.js
@@ -8,7 +8,7 @@ module.exports = () => {
         useUnifiedTopology: true
     }
     try{
-        mongoose.connect(process.env.DB_URL)
+        mongoose.connect("mongodb+srv://d_mart24:7815022995Gates@cluster0.qzizpsl.mongodb.net/?retryWrites=true&w=majority")
         console.log("The backend has connected to the MongoDB database.")
     } catch(error){
         console.log(`${error} could not connect`)
