ea226ef486a88c4d7e98bf9d48f1a581773e2e77
Daniel Rosen <danrosen6@gmail.com>
2023-04-04 14:32:42 -0400

    Added layer to live map that displays train stops Pulls location from MBTA api

--- frontend/src/components/pages/liveMap.js
+++ frontend/src/components/pages/liveMap.js
@@ -1,78 +1,86 @@
-import React, { useState,  useEffect} from 'react';
-import Map from 'ol/Map';
-import View from 'ol/View';
+import React, { useEffect, useRef } from 'react';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import 'ol/ol.css'
 import VectorLayer from 'ol/layer/Vector';
-import Style from 'ol/style/Style'
-import Icon from 'ol/style/Icon'
+import VectorSource from 'ol/source/Vector';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+import { Style, Icon } from 'ol/style';
+import axios from 'axios';
 
-/* Init is the map function 
-Map variable has view,center,zoom,minZoom,MaxZoom 
-view provides a 2d view of the map 
-center focuses where the user is looking at
-zoom is the magnification of the map
-TileLayter are prerendered grid tile images 
-that view displays to the user
+const MapComponent = () => {
+  // useRef is used to create a reference to the DOM element where the map will be rendered
+  const mapRef = useRef();
 
-*/
-const TrainMap = () => {
-  const[map, setMap] = useState();
-
+  // useEffect is used to perform side effects in function components
   useEffect(() => {
+    // Create a new vector source for the MBTA stops
+    const source = new VectorSource();
+
+    // Create a new vector layer with custom styling for the MBTA stops
+    const layer = new VectorLayer({
+      source: source,
+      style: new Style({
+        image: new Icon({
+          //src: 'frontend/src/icons/mbtaStations.png',
+          //scale: 0.5,
+          src: "https://openlayers.org/en/latest/examples/data/icon.png",
+          crossOrigin: 'anonymous'
+        }),
+      }),
+    });
+
+    // Initialize the map with a tile layer from OpenStreetMap and the custom vector layer for MBTA stops
     const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        layer,
+      ],
       view: new View({
         center: [-7910361.335273651, 5215196.272155075],
         zoom: 15,
         maxZoom: 20,
         minZoom: 10
       }),
-      layers: [
-        new TileLayer({
-        source: new OSM()
+    });
 
-          })
-      ],
-      target: 'map'
+    // Fetch MBTA stops data from the MBTA API and add them to the vector source as features
+    axios.get('https://api-v3.mbta.com/stops?filter[route_type]=2')
+      .then(response => {
+        response.data.data.forEach(stop => {
+          const coordinates = [
+            stop.attributes.longitude,
+            stop.attributes.latitude,
+          ];
+          const point = new Point(coordinates).transform('EPSG:4326', 'EPSG:3857');
+          const feature = new Feature({
+            geometry: point,
+          });
+          source.addFeature(feature);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching MBTA API:', error);
       });
-      setMap(map)
-      return () => {
-        map.setTarget(null);
-      };
-}, [])
- 
-useEffect(()=>{
-/*
-This useeffect will be to place the stations on their own vector layer on 
-the map
-*/
 
-})
+    // Clean up function to remove the map's target when the component is unmounted
+    return () => {
+      map.setTarget(null);
+    };
+  }, []);
 
-useEffect(() => {
- /*
- this use effect will be used to store the station and vehicle icons 
- new Style({
-    image: new Icon({
-      source: 'https://upload.wikimedia.org/wikipedia/commons/6/64/MBTA.svg',
-      size: [32, 32],
-      scale: 0.03     
-    })
-  })
-  */
-})
-  
- //display the map on the webpage
+  // Render a div that will contain the map
   return (
-    <div style={{height:'1080px', width:'1920px'}}id='map' class='map'></div>
-  
-  )
-}
+    <div
+      ref={mapRef}
+      style={{ width: '100%', height: '100vh', position: 'relative' }}
+    ></div>
+  );
+};
 
-export default  TrainMap;
-
-
-
-
-
+// Export the MapComponent for use in other parts of the application
+export default MapComponent;
