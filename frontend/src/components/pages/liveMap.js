5cc1caca3d2b60d55007c205f97542b6c33d5c89
Daniel Rosen <danrosen6@gmail.com>
2023-04-25 08:06:09 -0400

    Live Map filter

--- frontend/src/components/pages/liveMap.js
+++ frontend/src/components/pages/liveMap.js
@@ -13,24 +13,59 @@ import axios from 'axios';
 import { fromLonLat } from 'ol/proj';
 
 const TrainMap = () => {
-  // Create a reference to the DOM element where the map will be rendered
   const mapRef = useRef();
 
-  // Initialize state for the map and layer
   const [map, setMap] = useState();
   const [layer, setLayer] = useState(new TileLayer({ source: new OSM() }));
 
-  // Create a light mode layer for later use
   const lightModeLayer = new TileLayer({ source: new OSM() });
 
+  const [selectedTransit, setSelectedTransit] = useState("0");
+  const [selectedRoute, setSelectedRoute] = useState("");
+  const [routes, setRoutes] = useState([]);
+
   useEffect(() => {
-    // Create new vector sources for the MBTA stops, train locations, and subway locations
+    const fetchRoutes = async () => {
+      try {
+        const response = await axios.get(`https://api-v3.mbta.com/routes?filter[type]=${selectedTransit}`);
+        setRoutes(response.data.data);
+      } catch (error) {
+        console.error('Error fetching routes:', error);
+      }
+    };
+
+    fetchRoutes();
+  }, [selectedTransit]);
+
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    const initialMap = new Map({
+      target: mapRef.current,
+      layers: [layer],
+      view: new View({
+        center: [-7910361.335273651, 5215196.272155075],
+        zoom: 15,
+        maxZoom: 20,
+        minZoom: 10,
+      }),
+    });
+
+    setMap(initialMap);
+
+    return () => {
+      initialMap.setTarget(null);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!map) return;
+
     const trainSource = new VectorSource();
     const subwaySource = new VectorSource();
     const trainLocationsSource = new VectorSource();
     const subwayLocationsSource = new VectorSource();
 
-    // Create new vector layers with styling for commuter stops
     const trainStopLayer = new VectorLayer({
       source: trainSource,
       style: new Style({
@@ -41,7 +76,6 @@ const TrainMap = () => {
       }),
     });
 
-    // Create new vector layers with styling for subway stops
     const subwayStopLayer = new VectorLayer({
       source: subwaySource,
       style: new Style({
@@ -74,143 +108,101 @@ const TrainMap = () => {
       }),
     });
 
-    // Initialize the map with layers (tile layer from OpenStreetMap, custom vector layers for MBTA stops, train locations, and subway locations)
-    const map = new Map({
-      target: mapRef.current,
-      layers: [layer, subwayStopLayer, trainStopLayer, subwayLocationsLayer, trainLocationsLayer],
-      view: new View({
-        center: [-7910361.335273651, 5215196.272155075],
-        zoom: 15,
-        maxZoom: 20,
-        minZoom: 10,
-      }),
-    });
-    setMap(map);
+    // Clear the sources when switching transit types or routes
+    trainSource.clear();
+    subwaySource.clear();
+    trainLocationsSource.clear();
+    subwayLocationsSource.clear();
 
-    // Fetch MBTA stops data from the MBTA API and add them to the vector source as features
-    const fetchStops = (routeType, source) => {
-      axios
-        .get(`https://api-v3.mbta.com/stops?filter[route_type]=${routeType}`)
-        .then((response) => {
-          response.data.data.forEach((stop) => {
-            const coordinates = [
-              stop.attributes.longitude,
-              stop.attributes.latitude,
-            ];
-            const point = new Point(coordinates).transform(
-              'EPSG:4326',
-              'EPSG:3857'
-            );
-            const feature = new Feature({
-              geometry: point,
-            });
-            source.addFeature(feature);
+    const fetchStops = async () => {
+      try {
+        const stopResponse = await axios.get(`https://api-v3.mbta.com/stops?filter[route]=${selectedRoute}`);
+        stopResponse.data.data.forEach(stop => {
+          const coordinates = fromLonLat([parseFloat(stop.attributes.longitude), parseFloat(stop.attributes.latitude)]);
+          const stopPoint = new Point(coordinates);
+          const stopFeature = new Feature({
+            geometry: stopPoint,
           });
-        })
-        .catch((error) => {
-          console.error('Error fetching MBTA API:', error);
+
+          if (selectedTransit === "0") {
+            subwaySource.addFeature(stopFeature);
+          } else if (selectedTransit === "2") {
+            trainSource.addFeature(stopFeature);
+          }
         });
+      } catch (error) {
+        console.error('Error fetching stops:', error);
+      }
     };
 
-    // Fetch train and subway stops
-    fetchStops(2, trainSource);
-    fetchStops(0, subwaySource);
-    fetchStops(1, subwaySource);
+    const fetchLocations = async () => {
+      try {
+        const locationResponse = await axios.get(`https://api-v3.mbta.com/vehicles?filter[route]=${selectedRoute}`);
+        locationResponse.data.data.forEach(vehicle => {
+          const coordinates = fromLonLat([parseFloat(vehicle.attributes.longitude), parseFloat(vehicle.attributes.latitude)]);
+          const locationPoint = new Point(coordinates);
+          const locationFeature = new Feature({
+            geometry: locationPoint,
+          });
 
-    const updateTrainLocations = async () => {
-      const response = await axios.get('https://api-v3.mbta.com/vehicles?filter[route_type]=2');
-      const vehicles = response.data.data;
-
-      trainLocationsSource.clear();
-
-      vehicles.forEach((vehicle) => {
-        const coordinates = fromLonLat([parseFloat(vehicle.attributes.longitude), parseFloat(vehicle.attributes.latitude)]);
-        const point = new Point(coordinates);
-        const feature = new Feature(point);
-
-        trainLocationsSource.addFeature(feature);
-      });
+          if (selectedTransit === "0") {
+            subwayLocationsSource.addFeature(locationFeature);
+          } else if (selectedTransit === "2") {
+            trainLocationsSource.addFeature(locationFeature);
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching locations:', error);
+      }
     };
 
-    const updateSubwayLocations = async () => {
-      const response = await axios.get('https://api-v3.mbta.com/vehicles?filter[route_type]=0,1');
-      const vehicles = response.data.data;
-
-      subwayLocationsSource.clear();
-
-      vehicles.forEach((vehicle) => {
-        const coordinates = fromLonLat([parseFloat(vehicle.attributes.longitude), parseFloat(vehicle.attributes.latitude)]);
-        const point = new Point(coordinates);
-        const feature = new Feature(point);
-
-        subwayLocationsSource.addFeature(feature);
-      });
-    };
-
-    const intervalIdTrain = setInterval(updateTrainLocations, 7000);
-    const intervalIdSubway = setInterval(updateSubwayLocations, 7000);
-    updateTrainLocations();
-    updateSubwayLocations();
-
-    return () => {
-      clearInterval(intervalIdTrain);
-      clearInterval(intervalIdSubway);
-      map.setTarget(null);
-    };
-  }, [layer]);
-
-  function layerSwitch() {
-    if (layer.getSource() instanceof OSM) {
-      setLayer(
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-            tileSize: 512,
-          }),
-        })
-      );
-    } else {
-      setLayer(lightModeLayer);
+    if (selectedRoute) {
+      fetchStops();
+      fetchLocations();
     }
-  }
+
+    if (selectedTransit === "0") {
+      map.addLayer(subwayStopLayer);
+      map.addLayer(subwayLocationsLayer);
+      map.removeLayer(trainStopLayer);
+      map.removeLayer(trainLocationsLayer);
+    } else if (selectedTransit === "2") {
+      map.addLayer(trainStopLayer);
+      map.addLayer(trainLocationsLayer);
+      map.removeLayer(subwayStopLayer);
+      map.removeLayer(subwayLocationsLayer);
+    }
+
+  }, [map, selectedTransit, selectedRoute]);
 
   return (
-    <div style={{ height: '100vh', width: '100%' }} className="map">
-      <div
-        ref={mapRef}
-        id="map"
-        style={{ width: '100%', height: '100%' }}
-      ></div>
-      <div
-        className="form-check"
-        style={{
-          position: 'absolute',
-          top: '112px',
-          left: '10px',
-        }}
-      >
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="layer-toggle"
-          onChange={layerSwitch}
-          checked={layer.getSource() instanceof XYZ}
-          style={{
-            outline: 'none',
-            boxShadow: '0 0 10px rgba(0,0,0,0.5)',
-          }}
-        />
-        <label
-          className="form-check-label"
-          htmlFor="layer-toggle"
-          style={{
-            outline: 'none',
-            boxShadow: '0 0 10px rgba(0,0,0,0.5)',
-          }}
-        >
-          Dark Mode
-        </label>
-      </div>
+    <div>
+      <div ref={mapRef} style={{ height: '100vh', width: '100%' }}></div>
+<div style={{ position: 'absolute', top: '1rem', right: '1rem', zIndex: 1 }}>
+  <select
+    value={selectedRoute}
+    onChange={(e) => setSelectedRoute(e.target.value)}
+  >
+    <option value="">Select Route</option>
+    {routes.map((route) => (
+      <option key={route.id} value={route.id}>
+        {route.attributes.long_name}
+      </option>
+    ))}
+  </select>
+</div>
+<div style={{ position: 'absolute', top: '1rem', right: '16rem', zIndex: 1 }}>
+  <select
+    value={selectedTransit}
+    onChange={(e) => {
+      setSelectedTransit(e.target.value);
+      setSelectedRoute("");
+    }}
+  >
+    <option value="0">Subway</option>
+    <option value="2">Commuter Rail</option>
+  </select>
+</div>
     </div>
   );
 };
