cdcb639df81bf45112b1b11a3bd7ab3c183af498
Daniel Rosen <danrosen6@gmail.com>
2023-05-11 14:52:40 -0400

    Added comments to alert page

--- frontend/src/components/pages/alerts.js
+++ frontend/src/components/pages/alerts.js
@@ -1,34 +1,45 @@
+// Import required libraries and components
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 
+// Alerts component definition
 function Alerts() {
+  // State for storing alerts fetched from the API
   const [alerts, setAlerts] = useState([]);
 
+  // Fetch alerts using useEffect (executes once on component mount)
   useEffect(() => {
+    // Define fetchData as an async function for fetching data from the API
     async function fetchData() {
+      // Make a GET request to the API and store the response
       const result = await axios(
         'https://api-v3.mbta.com/alerts?sort=banner&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE',
       );
+      // Update the alerts state with the fetched data
       setAlerts(result.data.data);
     }
+    // Call fetchData function
     fetchData();
   }, []);
 
+  // Render the Alerts component
   return (
     <div 
-      style={{ 
-        display: 'flex', 
-        flexDirection: 'column', 
-        alignItems: 'center', 
-        padding: '2rem', 
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '2rem',
         backgroundImage: `url('/AlertPageOverlay.png')`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         height: '150vh'
       }}
     >
+      {/* Header for the alerts */}
       <h1 style={{color: 'white'}}>Recent Alerts</h1>
+      {/* Container for displaying the alerts */}
       <div 
         style={{
           height: '50vh',
@@ -39,6 +50,7 @@ function Alerts() {
           alignItems: 'center'
         }}
       >
+        {/* Loop through the alerts and render a Card component for each */}
         {alerts.map(alert => (
           <Card
             body
@@ -50,6 +62,7 @@ function Alerts() {
             style={{width: "30rem"}}
             key={alert.id}
           >
+            {/* Alert details (header and description) */}
             <Card.Body>
               <Card.Title>{alert.attributes.header}</Card.Title>
               <Card.Text>{alert.attributes.description}</Card.Text>
@@ -61,4 +74,5 @@ function Alerts() {
   );
 }
 
+// Export the Alerts component
 export default Alerts;
